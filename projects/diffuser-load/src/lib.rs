393605ca87480a73469a5bd44874e3ea81ddd507
Aster <galaster@foxmail.com>
2023-03-05 19:07:42 +0800

    Add python load script for test

--- projects/diffuser-load/src/lib.rs
+++ projects/diffuser-load/src/lib.rs
@@ -1,12 +1,35 @@
 mod errors;
 
-use safetensors::tensor::SafeTensors;
+use std::io::Read;
+use safetensors::tensor::{Dtype, SafeTensors, TensorView};
+
 pub use errors::{Error, Result};
 
+mod load_pickle;
+pub use crate::load_pickle::load_pth;
 
 
 #[test]
-fn test() {
-    let tnesor = SafeTensors::deserialize(include_bytes!("../schoolAnime_schoolAnime.safetensors")).unwrap();
-    println!("{:?}", tnesor.names());
+fn load_safe_tensors() {
+    let mut file = std::fs::File::open("schoolAnime_schoolAnime.safetensors").unwrap();
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).unwrap();
+    let meta = SafeTensors::read_metadata(&buffer).unwrap();
+    // let body = SafeTensors::deserialize(&buffer).unwrap();
+    // for (name, tensor) in body.tensors() {
+    //
+    // }
+    // TensorView::new();
+
+
+
+    println!("{:#?}", meta);
+    // println!("{:#?}", tnesor.names());
+}
+
+#[test]
+fn test2() {
+    let cm = tch::CModule::load("pureerosface_v1.pt").unwrap();
+
+    println!("{:#?}", cm.named_parameters());
 }
